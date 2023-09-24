1142fe72bfd3ba681a143fcd6fb67e62a219fb26
your name <you@example.com>
2023-09-24 16:30:36 -0700

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
+"""
+A script that starts a Flask web application:
+"""
 
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_HBNB():
+def hello_route():
     """
     Displays 'Hello HBNB!'
     Returns:
